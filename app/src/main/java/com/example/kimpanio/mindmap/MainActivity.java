8ecb0ad996e01347f1d61fad3c5065d8c75c6e6d
Kim <kn222gp@student.lnu.se>
2016-04-19 18:33:22 +0100

    Added add button and textfield. Can now add text to field below.

--- app/src/main/java/com/example/kimpanio/mindmap/MainActivity.java
+++ app/src/main/java/com/example/kimpanio/mindmap/MainActivity.java
@@ -1,15 +1,29 @@
 package com.example.kimpanio.mindmap;
 
+import android.app.ActionBar;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button addTextButton;
+    LinearLayout linearLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +32,39 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        editText = (EditText) findViewById(R.id.editTextField);
+        linearLayout = (LinearLayout) findViewById(R.id.mainLinearLayout);
+        addTextButton = (Button) findViewById(R.id.addTextButton);
+
+        addTextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                if (!TextUtils.isEmpty(editText.getText().toString())){
+                    TextView textView = new TextView(getApplicationContext());
+                    textView.setText(editText.getText());
+                    setTextViewStyle(textView);
+
+                    linearLayout.addView(textView, new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+                    editText.setText(emptyEditTextField(editText));
+
+                    Toast.makeText(view.getContext(), "Success!", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Toast.makeText(view.getContext(), "Fail!", Toast.LENGTH_LONG).show();
+                }
             }
         });
+    }
+
+    public void setTextViewStyle(TextView textView){
+        textView.setTextColor(Color.BLACK);
+        textView.setTextSize(20);
+        textView.setBackgroundColor(Color.CYAN);
+
+    }
+
+    public String emptyEditTextField(EditText editText){
+        return "";
     }
 
     @Override
